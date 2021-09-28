dc00ef7fc01425fe6fd67e2d023f2fa16a624bd5
unknown <apalmsaar@gmail.com>
2021-09-28 09:29:51 +0300

    Add While loop

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guess = 0;
+
+            Console.WriteLine("Guess a number between 1 and 10.");
+            Random random = new Random();
+            int correctNumber = random.Next(1, 11);
+
+            while (guess != correctNumber )
+            {
+                // get user input
+                string input = Console.ReadLine();
+
+                // Cast to int and put in guess
+                guess = Int32.Parse(input);
+
+                // match guess to correct number
+                if (guess != correctNumber)
+                {
+                    // set error message
+                    Console.WriteLine("Wrong number, try again");
+                }
+               
+            }
+            Console.WriteLine("You are Correct!");
+        }
+    }
+
+}
---
+++ WhileLoopRestricted/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace WhileLoopRestricted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guess = 0;
+
+            Console.WriteLine("Guess a number between 1 and 10.");
+            Random random = new Random();
+            int correctNumber = random.Next(1, 11);
+            correctNumber = 3;
+            int userTry = 0;
+            while (guess != correctNumber && userTry < 3)
+            {
+                // get user input
+                string input = Console.ReadLine();
+
+                // Cast to int and put in guess
+                guess = Int32.Parse(input);
+
+                // match guess to correct number
+                if (guess != correctNumber)
+                {
+                    // set error message
+                    Console.WriteLine($"You were incorrect, you have {2 - userTry} chances left");
+                    userTry++;
+                   
+                }
+                else if (guess == correctNumber)
+                {
+                    Console.WriteLine("You were correct!");
+                    break;
+                }
+
+            }
+            //Console.WriteLine("You ran out of chances");
+        }
+    }
+}
